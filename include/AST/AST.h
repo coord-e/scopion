86957d3ef44b0277d2a598952b62dbb02e71c96a
coord.e <me@coord-e.com>
2017-05-07 02:17:01 +0900

    AST printing support

--- include/AST/AST.h
+++ include/AST/AST.h
@@ -6,81 +6,131 @@
 #define BOOST_SPIRIT_USE_PHOENIX_V3 1
 
 #include <boost/variant.hpp>
+#include <iomanip>
 #include <iostream>
+#include <limits>
+#include <string>
+#include <vector>
 
-namespace scopion{
+namespace scopion {
 
 namespace ast {
 
-    struct add;
-    struct sub;
-    struct mul;
-    struct div;
-    struct rem;
-    struct shl;
-    struct shr;
-    struct iand;
-    struct ior;
-    struct ixor;
-    struct land;
-    struct lor;
-    struct eeq;
-    struct neq;
-    struct gt;
-    struct lt;
-    struct gtq;
-    struct ltq;
-    //struct assign;
+struct add;
+struct sub;
+struct mul;
+struct div;
+struct rem;
+struct shl;
+struct shr;
+struct iand;
+struct ior;
+struct ixor;
+struct land;
+struct lor;
+struct eeq;
+struct neq;
+struct gt;
+struct lt;
+struct gtq;
+struct ltq;
+// struct assign;
 
-    template <class Op>
-    struct binary_op;
+template <class Op> struct binary_op;
 
-    using value = boost::variant<
-        int,
-        bool,
-        boost::recursive_wrapper< std::string > //TODO: figure out why std::string needs wrapper to work
-    >;
+using value =
+    boost::variant<int, bool,
+                   boost::recursive_wrapper<std::string> // TODO: figure out why
+                                                         // std::string needs
+                                                         // wrapper to work
+                   >;
 
-    using expr = boost::variant<
-        value,
-        boost::recursive_wrapper< binary_op< add > >,
-        boost::recursive_wrapper< binary_op< sub > >,
-        boost::recursive_wrapper< binary_op< mul > >,
-        boost::recursive_wrapper< binary_op< div > >,
-        boost::recursive_wrapper< binary_op< rem > >,
-        boost::recursive_wrapper< binary_op< shl > >,
-        boost::recursive_wrapper< binary_op< shr > >,
-        boost::recursive_wrapper< binary_op< iand > >,
-        boost::recursive_wrapper< binary_op< ior > >,
-        boost::recursive_wrapper< binary_op< ixor > >,
-        boost::recursive_wrapper< binary_op< land > >,
-        boost::recursive_wrapper< binary_op< lor > >,
-        boost::recursive_wrapper< binary_op< eeq > >,
-        boost::recursive_wrapper< binary_op< neq > >,
-        boost::recursive_wrapper< binary_op< gt > >,
-        boost::recursive_wrapper< binary_op< lt > >,
-        boost::recursive_wrapper< binary_op< gtq > >,
-        boost::recursive_wrapper< binary_op< ltq > >
-        //boost::recursive_wrapper< binary_op< assign > >
-    >;
+using expr = boost::variant<value, boost::recursive_wrapper<binary_op<add>>,
+                            boost::recursive_wrapper<binary_op<sub>>,
+                            boost::recursive_wrapper<binary_op<mul>>,
+                            boost::recursive_wrapper<binary_op<div>>,
+                            boost::recursive_wrapper<binary_op<rem>>,
+                            boost::recursive_wrapper<binary_op<shl>>,
+                            boost::recursive_wrapper<binary_op<shr>>,
+                            boost::recursive_wrapper<binary_op<iand>>,
+                            boost::recursive_wrapper<binary_op<ior>>,
+                            boost::recursive_wrapper<binary_op<ixor>>,
+                            boost::recursive_wrapper<binary_op<land>>,
+                            boost::recursive_wrapper<binary_op<lor>>,
+                            boost::recursive_wrapper<binary_op<eeq>>,
+                            boost::recursive_wrapper<binary_op<neq>>,
+                            boost::recursive_wrapper<binary_op<gt>>,
+                            boost::recursive_wrapper<binary_op<lt>>,
+                            boost::recursive_wrapper<binary_op<gtq>>,
+                            boost::recursive_wrapper<binary_op<ltq>>
+                            // boost::recursive_wrapper< binary_op< assign > >
+                            >;
 
-    template <class Op>
-    struct binary_op
-    {
-        expr lhs;
-        expr rhs;
+template <class Op> struct binary_op {
+  expr lhs;
+  expr rhs;
 
-        binary_op(expr const& lhs_, expr const& rhs_) :
-            lhs( lhs_ ), rhs( rhs_ )
-        {
-          // if(lhs_.which() == 0 && rhs_.which() == 0){
-          //   std::cout << "/** Hello from binop ctor **/" << std::endl;
-          //   std::cout << "lhs = " << boost::get<int>(lhs_) << ", rhs = " << boost::get<int>(rhs_) << std::endl;
-          // }
-        }
-    };
+  binary_op(expr const &lhs_, expr const &rhs_) : lhs(lhs_), rhs(rhs_) {
+    // if(lhs_.which() == 0 && rhs_.which() == 0){
+    //   std::cout << "/** Hello from binop ctor **/" << std::endl;
+    //   std::cout << "lhs = " << boost::get<int>(lhs_) << ", rhs = " <<
+    //   boost::get<int>(rhs_) << std::endl;
+    // }
+  }
+};
+
+class printer : boost::static_visitor<void> {
+  std::ostream &_s;
+
+public:
+  explicit printer(std::ostream &s) : _s(s) {}
+
+  auto operator()(const value &v) const -> void {
+    _s << "[";
+    switch (v.which()) {
+    case 0: // int
+      std::cout << boost::get<int>(v);
+      break;
+    case 1: // bool
+      std::cout << std::boolalpha << boost::get<bool>(v);
+      break;
+    case 2: // string
+      std::cout << boost::get<std::string>(v);
+      break;
+    }
+    _s << "]";
+  }
+
+  template <typename T> auto operator()(const binary_op<T> &o) const -> void {
+    _s << "{ ";
+    boost::apply_visitor(printer(_s), o.lhs);
+    _s << " " << op_to_str(o) << " ";
+    boost::apply_visitor(printer(_s), o.rhs);
+    _s << " }";
+  }
+
+private:
+  std::string op_to_str(binary_op<add> const &) const { return "+"; }
+  std::string op_to_str(binary_op<sub> const &) const { return "-"; }
+  std::string op_to_str(binary_op<mul> const &) const { return "*"; }
+  std::string op_to_str(binary_op<div> const &) const { return "/"; }
+  std::string op_to_str(binary_op<rem> const &) const { return "%"; }
+  std::string op_to_str(binary_op<shl> const &) const { return "<<"; }
+  std::string op_to_str(binary_op<shr> const &) const { return ">>"; }
+  std::string op_to_str(binary_op<iand> const &) const { return "&"; }
+  std::string op_to_str(binary_op<ior> const &) const { return "|"; }
+  std::string op_to_str(binary_op<ixor> const &) const { return "^"; }
+  std::string op_to_str(binary_op<land> const &) const { return "&&"; }
+  std::string op_to_str(binary_op<lor> const &) const { return "||"; }
+  std::string op_to_str(binary_op<eeq> const &) const { return "=="; }
+  std::string op_to_str(binary_op<neq> const &) const { return "!="; }
+  std::string op_to_str(binary_op<gt> const &) const { return ">"; }
+  std::string op_to_str(binary_op<lt> const &) const { return "<"; }
+  std::string op_to_str(binary_op<gtq> const &) const { return ">="; }
+  std::string op_to_str(binary_op<ltq> const &) const { return "<="; }
+}; // class printer
 
 }; // namespace ast
-}; //namespace scopion
+}; // namespace scopion
 
-#endif //SCOPION_AST_H_
+#endif // SCOPION_AST_H_
--- src/scopion/scopion.cpp
+++ src/scopion/scopion.cpp
@@ -1,42 +1,45 @@
-#include <stdexcept>
-#include <iostream>
-#include <memory>
 #include <cstdio>
 #include <fstream>
 #include <functional>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
 
 #include "AST/AST.h"
-#include "Parser/Parser.h"
 #include "Assembly/Assembly.h"
+#include "Parser/Parser.h"
 
 #define SCOPION_VERSION "0.0.1-beta"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   std::cerr << "Welcome to scopion (" << SCOPION_VERSION << ")" << std::endl;
   std::string line;
-  while(1){
-      try {
-        std::cout << "> ";
-        std::getline(std::cin, line);
-        scopion::assembly asmb( "scopion_interpreter" );
-        auto ast = scopion::parser::parse_line(line);
-        asmb.IRGen({ast});
-        char* tmpname = strdup("/tmp/tmpfileXXXXXX");
-        mkstemp(tmpname);
-        std::string tmpstr(tmpname);
-        std::ofstream f(tmpstr);
-        f << asmb.getIR();
-        f.close();
-        if(system(("llc " + tmpstr).c_str()) != 0)
-          throw std::runtime_error( "Error occured while compiling IR" );
-        if(system(("gcc " + tmpstr + ".s -o " + tmpstr + "exec").c_str()) != 0)
-          throw std::runtime_error( "Error occured while compiling assembly" );
-        if(system((tmpstr + "exec").c_str()) != 0)
-          throw std::runtime_error( "Error occured while executing" );
-      }
-      catch( std::exception const& e ) {
-          std::cerr << "\033[1;31m[ERROR]\033[0m: " << e.what() << std::endl;
-      }
+  while (1) {
+    try {
+      std::cout << "> ";
+      std::getline(std::cin, line);
+      scopion::assembly asmb("scopion_interpreter");
+      auto ast = scopion::parser::parse_line(line);
+
+      std::cout << "AST: ";
+      boost::apply_visitor(scopion::ast::printer(std::cout), ast);
+      std::cout << std::endl;
+
+      asmb.IRGen({ast});
+      char *tmpname = strdup("/tmp/tmpfileXXXXXX");
+      mkstemp(tmpname);
+      std::string tmpstr(tmpname);
+      std::ofstream f(tmpstr);
+      f << asmb.getIR();
+      f.close();
+      if (system(("llc " + tmpstr).c_str()) != 0)
+        throw std::runtime_error("Error occured while compiling IR");
+      if (system(("gcc " + tmpstr + ".s -o " + tmpstr + "exec").c_str()) != 0)
+        throw std::runtime_error("Error occured while compiling assembly");
+      if (system((tmpstr + "exec").c_str()) != 0)
+        throw std::runtime_error("Error occured while executing");
+    } catch (std::exception const &e) {
+      std::cerr << "\033[1;31m[ERROR]\033[0m: " << e.what() << std::endl;
+    }
   }
 }
