d905f6abe56e7593dde656a1ef96e3441c8ed8af
coord.e <me@coord-e.com>
2017-08-20 16:27:42 +0900

    Refactoring of scopion::error to prevent from use-after-free segv

--- include/scopion/error.hpp
+++ include/scopion/error.hpp
@@ -13,37 +13,35 @@ class error : public std::runtime_error
 {
   using str_range_t = boost::iterator_range<std::string::const_iterator>;
 
-public:
-  const str_range_t where;
-  const str_range_t code;
-  const uint8_t level;
-
-  error(std::string const& message,
-        str_range_t const where_,
-        str_range_t const code_,
-        uint8_t level_ = 0)
-      : std::runtime_error(message), where(where_), code(code_), level(level_)
-  {
-  }
-
-  uint64_t line_number() const { return std::count(code.begin(), where.begin(), '\n'); }
-  str_range_t line_range() const
+  static str_range_t line_range(str_range_t const where, str_range_t const code)
   {
     auto range = boost::make_iterator_range(code.begin(), where.begin());
     auto sol   = boost::find(range | boost::adaptors::reversed, '\n').base();
     auto eol   = std::find(where.begin(), code.end(), '\n');
     return boost::make_iterator_range(sol, eol);
   }
-  std::string line() const
+  uint64_t line_number_;
+  uint64_t distance_;
+  std::string line_;
+  uint8_t level_;
+
+public:
+  error(std::string const& message,
+        str_range_t const where,
+        str_range_t const code,
+        uint8_t level = 0)
+      : std::runtime_error(message), level_(level)
   {
-    auto r = line_range();
-    return std::string(r.begin(), r.end());
+    line_number_ = std::count(code.begin(), where.begin(), '\n');
+    auto r       = line_range(where, code);
+    line_        = std::string(r.begin(), r.end());
+    distance_    = std::distance(r.begin(), where.begin());
   }
-  uint64_t distance() const
-  {
-    auto r = line_range();
-    return std::distance(r.begin(), where.begin());
-  }
+
+  uint64_t line_number() const { return line_number_; }
+  std::string line() const { return line_; }
+  uint64_t distance() const { return distance_; }
+  uint8_t level() const { return level_; }
 };
 
 };  // namespace scopion
