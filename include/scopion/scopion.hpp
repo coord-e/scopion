172cf59faed14dba37267f40dfc1caba1b707f26
coord.e <me@coord-e.com>
2017-08-24 03:13:53 +0900

    Add config.hpp.in to pass some information about compilation.
    Separate some CMakeLists.txt

--- include/scopion/scopion.hpp
+++ include/scopion/scopion.hpp
@@ -1,6 +1,8 @@
 #ifndef SCOPION_SCOPION_H_
 #define SCOPION_SCOPION_H_
 
+#include "scopion/config.hpp"
+
 #include "scopion/assembly/assembly.hpp"
 #include "scopion/ast/ast.hpp"
 #include "scopion/parser/parser.hpp"
